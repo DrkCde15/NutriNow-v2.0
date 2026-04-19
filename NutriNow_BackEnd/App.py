3091ea719b2ee5059b12c9e9b3fb7d0ec449c794
DrkCde15 <jcesarsantana215@gmail.com>
2026-04-19 12:04:46 -0300

    fix: restaura importação de redirect e finaliza JWT

--- NutriNow_BackEnd/App.py
+++ NutriNow_BackEnd/App.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
